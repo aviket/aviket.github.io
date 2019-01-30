4f59d76ae734dd40118f29404bf4615df85e8936
Avinash <aviket@users.noreply.github.com>
2019-01-30 02:03:52 -0500

    Add files via upload

---
+++ js/myjs.js
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*
+Note: You can easily create additional columns by adding more elements with the class of 'column' inside the 'columns' div. e.g.:
+
+...
+<div class="column">
+  <h2>4th column</h2>
+</div>
+<div class="column">
+  <h2>5th column</h2>
+</div>
+
+*/
